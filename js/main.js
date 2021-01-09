5b449f8a4dbeb6fcd8b03c62b34736efe5f29e87
Tomas Lopez Saavedra <tomas@lopezsaavedra.com.ar>
2021-01-09 20:02:35 -0300

    .

--- js/main.js
+++ js/main.js
@@ -1231,7 +1231,7 @@
         });
     
         function init(card) {
-            card.children(".audio-track").append('<button class="play"></button><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 100 100"><path id="circle" fill="none" stroke="#FFFFFF" stroke-miterlimit="10" d="M50,2.9L50,2.9C76,2.9,97.1,24,97.1,50v0C97.1,76,76,97.1,50,97.1h0C24,97.1,2.9,76,2.9,50v0C2.9,24,24,2.9,50,2.9z"/></svg>');
+            card.children(".audio-track").append('<button class="play"></button><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 100 100"><path id="circle" fill="none" stroke="#E6E6E6" stroke-miterlimit="10" d="M50,2.9L50,2.9C76,2.9,97.1,24,97.1,50v0C97.1,76,76,97.1,50,97.1h0C24,97.1,2.9,76,2.9,50v0C2.9,24,24,2.9,50,2.9z"/></svg>');
     
             var audio = card.find("audio"),
                 play = card.find('.play'),
